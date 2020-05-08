7c52a3a1f9bdcee26dc1f431b1eef37f283e5950
Jacob Lind <jacob.lind2@gmail.com>
2020-05-08 13:00:18 +0200

    updated h1 + added github connection

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>ravenous</h1>
+        <h1>Yelp copy project</h1>
         <SearchBar searchYelp={this.searchYelp}/>
         <BusinessList businesses={this.state.businesses} />
       </div>
