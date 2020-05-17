8221d8de8968dbf3cbc09a555c04b1561e0e02a8
Jacob Lind <jacob.lind2@gmail.com>
2020-05-17 13:55:54 +0200

    changed name to ravenous

---
+++ src/Views/Jammming/jammming.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class componentName extends Component {
+    componentDidMount() {
+        const favicon = document.getElementById('favicon');
+        favicon.href = './jammming.ico';
+      }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default componentName;
