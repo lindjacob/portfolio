92b052210821949bca9aa9946292c88c6ff762db
Jacob Lind <jacob.lind2@gmail.com>
2020-09-03 18:00:48 +0200

    added GitHub link

--- src/Views/Resume/Resume.js
+++ src/Views/Resume/Resume.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Resume.css';
 
 class Resume extends Component {
     componentDidMount() {
@@ -10,6 +11,7 @@ class Resume extends Component {
         return (
             <div>
                 <h1>My resume is currently under development...</h1>
+                <h3><a href="https://github.com/lindjacob/portfolio">Link til GitHub repository</a></h3>
             </div>
         );
     }
